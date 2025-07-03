d41bd1d5d1febeb9559b194aac90fbd218a4707b
sindri <147028060+sindricn@users.noreply.github.com>
2025-07-03 20:54:26 +0800

    审核通过: 添加站点 nbvil

--- src/websiteData.js
+++ src/websiteData.js
@@ -674,6 +674,16 @@ export const websiteData = [
     ],
     "icon": "/assets/dev_tools_icon.png",
     "popularity": 92
+  },
+  {
+    "id": 51,
+    "name": "nbvil",
+    "description": "gg",
+    "url": "https://blog.nbvil.com",
+    "category": "friend_links",
+    "tags": [],
+    "icon": "/assets/tools_icon.png",
+    "popularity": 50
   }
 ];
 
