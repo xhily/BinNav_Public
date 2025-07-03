9b2e434550c931acd59e6926e6e16d05488c3ff1
sindri <147028060+sindricn@users.noreply.github.com>
2025-07-03 18:00:03 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { useEffect } from 'react'
 import HomePage from './pages/HomePage.jsx'
 import Admin from './pages/Admin.jsx'
-import { useSiteConfig } from './hooks/useSiteConfig.js'
 import './App.css'
 
 function App() {
-  const { siteConfig } = useSiteConfig()
-  
-  // 初始化页面标题
-  useEffect(() => {
-    document.title = siteConfig.siteTitle
-  }, [siteConfig.siteTitle])
-
   return (
     <BrowserRouter>
       <Routes>
