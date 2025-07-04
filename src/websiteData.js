61d3889ca89a96d8bca9f9bc490ab3dfd1529243
sindri <147028060+sindricn@users.noreply.github.com>
2025-07-04 10:42:09 +0800

    🔧 Auto-update website config via EdgeOne Functions

--- src/websiteData.js
+++ src/websiteData.js
@@ -1,39 +1,23 @@
-// 网站数据 - 通过管理后台更新于 2025/7/4 10:29:00
+// 网站数据 - 通过管理后台更新于 2025/7/4 10:42:06
 export const websiteData = [
   {
     "id": 40,
-    "name": "Navigator Blog",
-    "description": "技术分享与前端开发心得",
-    "url": "https://navigator-blog.dev",
+    "name": "Newbie Village",
+    "description": "二进制博客",
+    "url": "https://blog.nbvil.com/",
     "category": "author",
     "tags": [
       "个人博客",
-      "前端技术",
+      "教程分享",
       "开发心得"
     ],
-    "icon": "/assets/tech_blogger_avatar.png",
-    "popularity": 95,
-    "featured": true
-  },
-  {
-    "id": 43,
-    "name": "微信公众号",
-    "description": "「前端导航」| 每日推荐优质技术资源",
-    "url": "https://mp.weixin.qq.com/navigator",
-    "category": "author",
-    "tags": [
-      "微信公众号",
-      "技术分享",
-      "前端资源"
-    ],
-    "icon": "/assets/social_icon.png",
-    "popularity": 88
+    "icon": "/default_icon.png"
   },
   {
     "id": 45,
     "name": "知乎专栏",
-    "description": "「现代前端开发」专栏作者",
-    "url": "https://zhuanlan.zhihu.com/navigator-dev",
+    "description": "二进制-知乎",
+    "url": "https://www.zhihu.com/people/mrblack-41-9",
     "category": "author",
     "tags": [
       "知乎专栏",
@@ -42,297 +26,26 @@ export const websiteData = [
     "icon": "/default_icon.png"
   },
   {
-    "id": 50,
-    "name": "Vue.js",
-    "description": "渐进式JavaScript框架",
-    "url": "https://vuejs.org",
-    "category": "category_1751550177044",
+    "id": 1751596898467,
+    "name": "B站",
+    "description": "作者B站频道，不定期上传视频教程",
+    "url": "https://space.bilibili.com/3546865807133625",
+    "category": "author",
     "tags": [
-      "JavaScript",
-      "框架",
-      "前端"
+      "视频"
     ],
     "icon": "/default_icon.png"
   },
   {
     "id": 41,
     "name": "GitHub Projects",
-    "description": "开源项目集合 | React & Vue 组件库",
-    "url": "https://github.com/navigator-dev",
+    "description": "作者开源项目",
+    "url": "https://github.com/sindricn",
     "category": "recommended",
     "tags": [
-      "开源项目",
-      "React",
-      "Vue",
-      "组件库"
+      "开源项目"
     ],
     "icon": "/default_icon.png"
-  },
-  {
-    "id": 3,
-    "name": "UI中国",
-    "description": "图形交互与界面设计交流、作品展示、学习平台",
-    "url": "https://ui.cn",
-    "category": "recommended",
-    "tags": [
-      "UI设计",
-      "交互设计",
-      "中文"
-    ],
-    "icon": "/assets/tools_icon.png",
-    "popularity": 88
-  },
-  {
-    "id": 4,
-    "name": "站酷",
-    "description": "中国人气设计师互动平台",
-    "url": "https://zcool.com.cn",
-    "category": "recommended",
-    "tags": [
-      "设计",
-      "创意",
-      "中文平台"
-    ],
-    "icon": "/assets/tools_icon.png",
-    "popularity": 85
-  },
-  {
-    "id": 5,
-    "name": "Pinterest",
-    "description": "全球美图收藏采集站",
-    "url": "https://pinterest.com",
-    "category": "recommended",
-    "tags": [
-      "灵感",
-      "图片",
-      "收藏"
-    ],
-    "icon": "/assets/social_icon.png",
-    "popularity": 90
-  },
-  {
-    "id": 10,
-    "name": "Collect UI",
-    "description": "每日UI设计灵感收集",
-    "url": "https://collectui.com",
-    "category": "ui_inspiration",
-    "tags": [
-      "UI设计",
-      "每日灵感",
-      "界面"
-    ],
-    "icon": "/assets/tools_icon.png",
-    "popularity": 88
-  },
-  {
-    "id": 11,
-    "name": "Android Niceties",
-    "description": "精美Android应用界面截图收集",
-    "url": "https://androidniceties.tumblr.com",
-    "category": "ui_inspiration",
-    "tags": [
-      "Android",
-      "应用界面",
-      "移动设计"
-    ],
-    "icon": "/assets/tools_icon.png",
-    "popularity": 80
-  },
-  {
-    "id": 12,
-    "name": "Awwwards",
-    "description": "全球顶级网页设计奖项和灵感",
-    "url": "https://awwwards.com",
-    "category": "web_inspiration",
-    "tags": [
-      "网页设计",
-      "奖项",
-      "创意"
-    ],
-    "icon": "/assets/network_icon.png",
-    "popularity": 95
-  },
-  {
-    "id": 13,
-    "name": "CSS Design Awards",
-    "description": "网站设计奖项与灵感画廊",
-    "url": "https://cssdesignawards.com",
-    "category": "web_inspiration",
-    "tags": [
-      "CSS",
-      "网页设计",
-      "奖项"
-    ],
-    "icon": "/assets/network_icon.png",
-    "popularity": 88
-  },
-  {
-    "id": 14,
-    "name": "Site Inspire",
-    "description": "CSS画廊和最佳网页设计灵感展示",
-    "url": "https://siteinspire.com",
-    "category": "web_inspiration",
-    "tags": [
-      "网页设计",
-      "CSS",
-      "灵感"
-    ],
-    "icon": "/assets/network_icon.png",
-    "popularity": 85
-  },
-  {
-    "id": 15,
-    "name": "Iconfinder",
-    "description": "210万+ 免费和付费矢量图标",
-    "url": "https://iconfinder.com",
-    "category": "icons",
-    "tags": [
-      "图标",
-      "矢量",
-      "免费"
-    ],
-    "icon": "/assets/.png",
-    "popularity": 92
-  },
-  {
-    "id": 16,
-    "name": "iconfont",
-    "description": "阿里巴巴矢量图标库",
-    "url": "https://iconfont.cn",
-    "category": "icons",
-    "tags": [
-      "图标",
-      "阿里巴巴",
-      "中文"
-    ],
-    "icon": "/assets/.png",
-    "popularity": 95
-  },
-  {
-    "id": 17,
-    "name": "Iconmonstr",
-    "description": "免费简洁图标素材",
-    "url": "https://iconmonstr.com",
-    "category": "icons",
-    "tags": [
-      "免费图标",
-      "简洁",
-      "SVG"
-    ],
-    "icon": "/assets/.png",
-    "popularity": 85
-  },
-  {
-    "id": 18,
-    "name": "Flaticon",
-    "description": "63万+ 免费矢量图标",
-    "url": "https://flaticon.com",
-    "category": "icons",
-    "tags": [
-      "扁平图标",
-      "免费",
-      "矢量"
-    ],
-    "icon": "/assets/.png",
-    "popularity": 90
-  },
-  {
-    "id": 19,
-    "name": "365PSD",
-    "description": "免费PSD和图形、插画素材",
-    "url": "https://365psd.com",
-    "category": "design_resources",
-    "tags": [
-      "PSD",
-      "免费素材",
-      "图形"
-    ],
-    "icon": "/assets/.png",
-    "popularity": 80
-  },
-  {
-    "id": 20,
-    "name": "Freepik",
-    "description": "超过百万免费矢量、PSD、照片和图标",
-    "url": "https://freepik.com",
-    "category": "design_resources",
-    "tags": [
-      "矢量",
-      "PSD",
-      "照片"
-    ],
-    "icon": "/assets/.png",
-    "popularity": 92
-  },
-  {
-    "id": 21,
-    "name": "Unsplash",
-    "description": "免费高质量图片素材",
-    "url": "https://unsplash.com",
-    "category": "design_resources",
-    "tags": [
-      "摄影",
-      "免费",
-      "高质量"
-    ],
-    "icon": "/assets/.png",
-    "popularity": 95
-  },
-  {
-    "id": 22,
-    "name": "Pexels",
-    "description": "免费图片和视频素材",
-    "url": "https://pexels.com",
-    "category": "design_resources",
-    "tags": [
-      "图片",
-      "视频",
-      "免费"
-    ],
-    "icon": "/assets/.png",
-    "popularity": 88
-  },
-  {
-    "id": 30,
-    "name": "GitHub",
-    "description": "全球最大的代码托管平台",
-    "url": "https://github.com",
-    "category": "dev_tools",
-    "tags": [
-      "代码托管",
-      "开源",
-      "协作"
-    ],
-    "icon": "/assets/.png",
-    "popularity": 98
-  },
-  {
-    "id": 31,
-    "name": "CodePen",
-    "description": "前端代码分享平台",
-    "url": "https://codepen.io",
-    "category": "dev_tools",
-    "tags": [
-      "前端",
-      "代码分享",
-      "在线编辑"
-    ],
-    "icon": "/assets/.png",
-    "popularity": 90
-  },
-  {
-    "id": 32,
-    "name": "Stack Overflow",
-    "description": "程序员问答社区",
-    "url": "https://stackoverflow.com",
-    "category": "dev_tools",
-    "tags": [
-      "问答",
-      "编程",
-      "技术支持"
-    ],
-    "icon": "/assets/.png",
-    "popularity": 95
   },
   {
     "id": 33,
